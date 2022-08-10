3255ef12e24c7e53832df3744af836bcbbd1f03c
XDevant <xdevant@gmail.com>
2022-08-10 12:40:14 +0200

    db, create user, add user and edit user working in /admin

--- EpicEvents/EpicEvents/authentication/admin.py
+++ EpicEvents/EpicEvents/authentication/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
+from .forms import UserChangeForm, UserCreationForm
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active')
+    list_filter = ('role',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name',)}),
+        ('Permissions', {'fields': ('role', 'is_active',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'last_name',)
+    ordering = ('last_name',)
+    filter_horizontal = ()
 
 
 admin.site.register(User, UserAdmin)
--- EpicEvents/EpicEvents/authentication/models.py
+++ EpicEvents/EpicEvents/authentication/models.py
@@ -1,6 +1,59 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 
 
-class User(AbstractUser):
-    pass
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+        if extra_fields.get("role") != "admin":
+            raise ValueError("Superuser must have role='admin'.")
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser):
+    class Role(models.TextChoices):
+        NONE = 'none'
+        ADMIN = 'admin'
+        SUPPORT = 'support'
+        COMMERCIAL = 'commercial'
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    role = models.CharField(choices=Role.choices, max_length=16, default='none')
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} couriel:{self.email}"
+
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        return self.is_superuser or self.role == "admin"
+
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
+
+    def has_module_perms(self, app_label):
+        return self.is_staff
