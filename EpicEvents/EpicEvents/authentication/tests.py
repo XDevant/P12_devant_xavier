9ebf08b0dcc139996ede90addac91a3cc387096b
XDevant <xdevant@gmail.com>
2022-09-07 13:12:08 +0200

    test user working

--- EpicEvents/EpicEvents/authentication/tests.py
+++ EpicEvents/EpicEvents/authentication/tests.py
@@ -1,41 +1,43 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 
 
 def create_test_users(apps, schema_migration):
     """Used in authentication.migrations.0002_auto... to create our test users"""
     user = get_user_model()
+
     user.objects.create(
         first_name="De",
         last_name="De",
         email="de@de.co",
         role="sales",
-        password="mdp1"
+        password=make_password("mdp1")
     )
     user.objects.create(
         first_name="Do",
         last_name="Do",
-        email="dà@do.co",
+        email="do@do.co",
         role="sales",
-        password="mdp2"
+        password=make_password("mdp2")
     )
     user.objects.create(
         first_name="Bi",
         last_name="Bi",
         email="bi@bi.co",
         role="support",
-        password="mdp3"
+        password=make_password("mdp3")
     )
     user.objects.create(
         first_name="Bo",
         last_name="Bo",
         email="bo@bo.co",
         role="support",
-        password="mdp3"
+        password=make_password("mdp4")
     )
     user.objects.create(
       first_name="Za",
       last_name="Za",
       email="za@za.co",
       role="admin",
-      password="mdp5"
+      password=make_password("mdp5")
     )
